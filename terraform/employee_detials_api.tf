09e2b37353fb6a8481d740302c29fbc6e7f2649c
Michael Humphries <michaelhumphries1997@gmail.com>
2019-11-04 19:23:52 +0000

    saving stuff

--- terraform/employee_detials_api.tf
+++ terraform/employee_detials_api.tf
@@ -2,3 +2,16 @@ resource "aws_api_gateway_rest_api" "details_api" {
   name        = "EmployeeDetailsApi"
   description = "This is my API for interfacing with an employees database"
 }
+
+resource "aws_api_gateway_resource" "customer_details_endpoint" {
+  rest_api_id = "${aws_api_gateway_rest_api.details_api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.details_api.root_resource_id}"
+  path_part   = "getCustomerDetailsById"
+}
+
+resource "aws_api_gateway_method" "get_customer_details" {
+  rest_api_id   = "${aws_api_gateway_rest_api.details_api.id}"
+  resource_id   = "${aws_api_gateway_resource.customer_details_endpoint.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,10 +1,14 @@
 #!bin/bash
 RED='\033[0;31m'
 WHITE='\e[39m'
+echo -e "${RED}*** getting caller identity ***${WHITE}"
 aws sts get-caller-identity
 echo -e "${RED}*** listing buckets ***${WHITE}"
 aws s3 ls
+echo -e "${RED}*** building lambda ***${WHITE}"
 bash build.sh
 cd ../terraform
+echo -e "${RED}*** initializing terraform ***${WHITE}"
 terraform init
+echo -e "${RED}*** deploying infrastructure ***${WHITE}"
 terraform apply
